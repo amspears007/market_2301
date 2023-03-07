10b8c5cc8c10603f56f6a87dd732f524a59ba671
Amy Spears <amspears007@gmail.com>
2023-03-07 09:26:17 -0700

    Add 2nd it block in vendor spec

--- spec/vendor_spec.rb
+++ spec/vendor_spec.rb
@@ -1,8 +1,15 @@
 require 'spec_helper'
 
 RSpec.describe Vendor do
+  let(:vendor)  {Vendor.new("Rocky Mountain Fresh")}
+  
   it 'exists and has a name' do
     vendor = Vendor.new("Rocky Mountain Fresh")
     expect(vendor.name).to eq("Rocky Mountain Fresh")
+  end
+
+  it 'starts with no inventory' do
+    expect(vendor.inventory).to eq({})
+    expect(vendor.check_stock(item)).to eq(0)
   end
 end
