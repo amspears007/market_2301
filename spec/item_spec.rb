cbd40e32a9b857ca78767840a8fe8f026c046f15
Amy Spears <amspears007@gmail.com>
2023-03-07 09:19:45 -0700

    Update items spec to two it blocks and let blocks

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 RSpec.describe Item do
-  it 'exists and has a name and price' do
+    let(:item1) {Item.new({name: 'Peach', price: "$0.75"})}
+    let(:item2) {Item.new({name: 'Tomato', price: '$0.50'})}
+    
+  it 'exists' do
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
     expect(item2).to be_a(Item) 
+  end
+
+  it 'has a name and price' do
     expect(item2.name).to eq('Tomato')
     expect(item2.price).to eq(0.5)
-  end
-end 
+  end 
+end
