d7172f4d782974e62c4e2e130ce12d0d23a71777
Amy Spears <amspears007@gmail.com>
2023-03-07 09:15:53 -0700

    Add name and pirce tests with updated price attribute in Item class

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe Item do
-  it 'exists' do
+  it 'exists and has a name and price' do
     item1 = Item.new({name: 'Peach', price: "$0.75"})
     item2 = Item.new({name: 'Tomato', price: '$0.50'})
-    expect(item1).to be_a(Item) 
+    expect(item2).to be_a(Item) 
+    expect(item2.name).to eq('Tomato')
+    expect(item2.price).to eq(0.5)
   end
 end 
